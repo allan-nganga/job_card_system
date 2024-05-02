606f6b72afa0996226ab76f4a4cd6f6d7a682314
allan-nganga <allen.nganga7@gmail.com>
2024-05-02 12:22:51 +0300

    Added static file to store css, added templates folder to settings

--- job_card_system/settings.py
+++ job_card_system/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'job_card_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# To redirect user upon successful login
+LOGIN_REDIRECT_URL = '/'
